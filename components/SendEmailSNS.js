ff2d1fb940f22db1508e3d60ca41a37f5ecc6e7b
Veronica Gomez Tirado <veronica.gomez@vasslatam.com>
2022-07-10 16:50:45 -0500

    agregar servicio de correo

---
+++ components/SendEmailSNS.js
@@ -0,0 +1,21 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-central-1'});
+
+// Create publish parameters
+var params = {
+    Message: 'Chequeo pre operacional diligenciado', /* required */
+    TopicArn: 'arn:aws:sns:eu-central-1:983052191492:Correo'
+  };
+  
+  // Create promise and SNS service object - no es necesario
+  var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+  
+  // Handle promise's fulfilled/rejected states
+  publishTextPromise.then(
+    function(data) {
+      console.log(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
+      console.log("MessageID is " + data.MessageId);
+    }).catch(
+      function(err) {
+      console.error(err, err.stack);
+    });
---
+++ utils/SendEmail.js
@@ -0,0 +1,5 @@
+export default {
+    //...others
+    awsClientID: process.env. AWS_ACCESS_KEY_ID,
+    awsClientSecret: process.env.AWS_SECRET_ACCESS_KEY
+  }
